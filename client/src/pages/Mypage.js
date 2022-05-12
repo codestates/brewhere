ee5f17bc630694dcb2254207bcdc56698d13c64c
kitch-finn <fin@kakao.com>
2022-05-12 21:26:36 +0900

    base fix

---
+++ client/src/pages/Mypage.js
@@ -0,0 +1,10 @@
+import React, {  } from 'react';
+
+export default function Mypage() {
+
+
+
+return (
+  <div>Mypage</div>
+  );
+}
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,10 @@
+import React, {  } from 'react';
+
+export default function Signup() {
+
+
+
+return (
+  <div>Signup</div>
+  );
+}
