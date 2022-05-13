c18a4b4d430f7354a2826d8a41003bbe71aef4e5
hsly22xk <hsly22xk@naver.com>
2022-05-13 13:26:14 +0900

    server all commit

---
+++ server/controllers/tokenFunctions/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { sign, verify } = require('jsonwebtoken');
+
+module.exports = {
+  generateAccessToken: data => {
+    return sign(data, process.env.ACCESS_SECRET, { expiresIn: '15s' });
+  },
+
+  generateRefreshToken: data => {
+    return sign(data, process.env.REFRESH_SECRET, { expiresIn: '10d' });
+  },
+
+  sendRefreshToken: (res, refreshToken) => {
+    res.cookie('refreshToken', refreshToken, {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    });
+  },
+
+  sendAccessToken: (res, accessToken) => {
+    res.json({ accessToken: accessToken, message: '로그인 되었습니다' });
+  },
+
+  checkAccessToken: req => {
+    const authorization = req.headers['authorization'];
+    if (!authorization) {
+      return null;
+    }
+    const token = authorization.split(' ')[1];
+    try {
+      return verify(token, process.env.ACCESS_SECRET);
+    } catch (err) {
+      return null;
+    }
+  },
+  
+  checkRefreshToken: refreshToken => {
+    try {
+      return verify(refreshToken, process.env.REFRESH_SECRET);
+    } catch (err) {
+      return null;
+    }
+  },
+};
---
+++ server/controllers/users/signup.js
@@ -0,0 +1,26 @@
+const { user } = require('../models');
+const { generateAccessToken } = require('../controllers/tokenFunctions');
+
+module.exports = (req, res) => {
+  const {user_name, user_email, password} = req.body;
+
+  if(!user_name || !user_email || !password) {
+    res.status(400).json({message: '모든 항목은 필수입니다'})
+  } else {
+    await user.findOrCreate({
+      where: {user_email: user_email},
+      default: {user_name, password}
+    }).then(([ , created]) => {
+      if(!created) {
+        res.status(409).json({message: '이미 등록된 이메일입니다'})
+      } else {
+        const accessToken = generateAccessToken({id, user_email, password, user_name, created_at, updated_at});
+        return res.status(201).cookie('jwt', accessToken, {
+          httpOnly: 'true',
+          sameSite: 'none',
+          secure: 'true'
+        }).json({message: '회원가입 성공'});
+      }
+    })
+  }
+}
