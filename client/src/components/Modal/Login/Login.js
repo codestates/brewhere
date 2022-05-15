69dc168b56d81152a6ceb2e497d2ba9e241fa1f0
kitch-finn <fin@kakao.com>
2022-05-15 19:10:02 +0900

    set pages detail

--- client/src/components/Modal/Login/Login.js
+++ client/src/components/Modal/Login/Login.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import logo from '../../Landing/img/logo_x05_square.png'
+import './Login.css'
+
+Modal.setAppElement('#root')
+
 
 function Login () {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  // 모달 창이 열렸을 때, 스크롤 막기
+  if (modalIsOpen) {
+    document.body.style.overflow = "hidden";
+  } else { document.body.style.overflow = "unset" }
+
   return (
     <>
       <button onClick={()=> setModalIsOpen(!modalIsOpen)}>로그인</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} >
-        This is Login content
-        <button onClick={()=> setModalIsOpen(!modalIsOpen)}>close</button>
-      </Modal>
+        <div className="modal-login" >
+          <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} >
+            {/* 
+            이메일, 비밀번호로 로그인 - DB와 매치
+            카카오 Oauth 로그인 
+            회원가입 페이지 링크
+            */}
+            <img 
+              className="modal-login-logo"
+            src={ logo } alt='logo' />
+            <button onClick={()=> setModalIsOpen(!modalIsOpen)}>close</button>
+            <div>이메일</div>
+            <input className="input-login" type="text" placeholder="example@example.com" />
+            <div>비밀번호</div>
+            <input className="input-login" type="text" placeholder="8자 이상의 영문, 숫자를 입력해주세요" />
+          </Modal>
+      </div>
     </>
   )
 }  
