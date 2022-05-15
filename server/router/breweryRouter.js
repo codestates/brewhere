02386401f539849d0fe0b6d8a01260fc4499bb84
YeonSeoJo <irwinjoebeck@gmail.com>
2022-05-15 11:01:06 +0900

    why merge error happend

--- server/router/breweryRouter.js
+++ server/router/breweryRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const breweryRouter = require('../controllers/brewery');
+const breweryRouter = require('../controllers/Brewery');
 
 router.get('/:breweryid', breweryRouter.breweryList);
 
