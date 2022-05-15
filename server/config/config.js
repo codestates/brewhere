ad29f69d868e6c444ae484cbabdfc189e7a3162f
YeonSeoJo <irwinjoebeck@gmail.com>
2022-05-15 14:13:14 +0900

    migration edited

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   development: {
     username: "root",
-    password: process.env.DATABASE_PASSWORD,
+    password: "1234",
     database: "brewhere",
     host: "127.0.0.1",
     dialect: "mysql",
