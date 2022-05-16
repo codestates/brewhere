b02688bc68469c4cc3c5eba79a1934c897e141c1
D-MG-lab <jmingyu92@gmail.com>
2022-05-17 04:42:46 +0900

    create map marker and info

--- client/src/components/Modal/Login/Login.js
+++ client/src/components/Modal/Login/Login.js
@@ -1,22 +1,17 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import logo from '../../Landing/img/logo_x05_square.png'
-import styled from 'styled-components';
-import './Login.css'
-
-Modal.setAppElement('#root')
-
-
+import React, { useState } from "react";
+import logo from "../../Landing/img/logo_x05_square.png";
+import styled from "styled-components";
+import "./Login.css";
 
 export const ModalContainer = styled.div`
-// TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
+  // TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
   text-align: center;
   width: 100%;
   height: 100%;
 `;
 
 export const ModalBackdrop = styled.div`
- // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
+  // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
   position: fixed;
   display: flex;
   justify-content: center;
@@ -32,7 +27,6 @@ export const ModalBackdrop = styled.div`
   z-index: 999; // 레이어의 맨 위에 모달 창이 나와야 하므로 가장 큰 수(999)로 설정
 `;
 
-
 export const ModalBtn = styled.button`
   background-color: #4000c7;
   text-decoration: none;
@@ -43,9 +37,9 @@ export const ModalBtn = styled.button`
   cursor: grab;
 `;
 
-export const ModalView = styled.div.attrs(props => ({
+export const ModalView = styled.div.attrs((props) => ({
   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
-  role: 'dialog'
+  role: "dialog",
 }))`
   display: flex;
   flex-direction: column;
@@ -56,35 +50,37 @@ export const ModalView = styled.div.attrs(props => ({
   height: 600px;
   border-radius: 1rem;
   position: relative;
-  > div.close-btn{
+  > div.close-btn {
     position: absolute;
-    top:2px;
-    right:7px;
+    top: 2px;
+    right: 7px;
     cursor: pointer; // 마우스포인터를 위에 올리면 커서가 손 모양으로 변하게
-  > div.desc {
-    color: violet;
-    margin-top: 25px;
-  }
+    > div.desc {
+      color: violet;
+      margin-top: 25px;
+    }
   }
 `;
 
-function Login () {
+function Login() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const openModalHandler = () => {
-    setModalIsOpen(!modalIsOpen)
+    setModalIsOpen(!modalIsOpen);
   };
 
   // 모달 창이 열렸을 때, 스크롤 막기
   if (modalIsOpen) {
     document.body.style.overflow = "hidden";
-  } else { document.body.style.overflow = "unset" }
-  
+  } else {
+    document.body.style.overflow = "unset";
+  }
+
   // 카카오 로그인 관련
-  const CLIENT_ID = 'a879c6361070a85ff535c43fddfd2bba';
-  const REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao'
+  const CLIENT_ID = "a879c6361070a85ff535c43fddfd2bba";
+  const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
- 
+
   return (
     <>
       <ModalContainer onClick={openModalHandler}>
@@ -93,37 +89,47 @@ function Login () {
           로그인
         </ModalBtn>
 
-        { modalIsOpen ? <ModalBackdrop onClick={openModalHandler}>
-                    <ModalView onClick={(event) => {
-                      event.stopPropagation()
-                    }
-                  }>
-                      <div className="close-btn" onClick={openModalHandler}>
-                        &times;
-                      </div>
+        {modalIsOpen ? (
+          <ModalBackdrop onClick={openModalHandler}>
+            <ModalView
+              onClick={(event) => {
+                event.stopPropagation();
+              }}
+            >
+              <div className="close-btn" onClick={openModalHandler}>
+                &times;
+              </div>
 
-                      <div className="desc">
-                        <img src={logo} alt="modal-login-logo" className="modal-login-logo"/>
-                      </div>
-                      <div className="desc">이메일</div>
-                      <input type="text" className='input-login' placeholder="example@example.com" />
-                      <div className="desc">비밀번호</div>
-                      <input type="password" className='input-login' placeholder="8자 이상의 비밀번호를 입력해주세요" />
-                      <button className="desc login-btn">로그인</button>
+              <div className="desc">
+                <img
+                  src={logo}
+                  alt="modal-login-logo"
+                  className="modal-login-logo"
+                />
+              </div>
+              <div className="desc">이메일</div>
+              <input
+                type="text"
+                className="input-login"
+                placeholder="example@example.com"
+              />
+              <div className="desc">비밀번호</div>
+              <input
+                type="password"
+                className="input-login"
+                placeholder="8자 이상의 비밀번호를 입력해주세요"
+              />
+              <button className="desc login-btn">로그인</button>
 
-                      <a href={KAKAO_AUTH_URL}>
-                        <div 
-                            className="kakao_btn"
-                            >
-                        </div>
-                      </a>
-
-                    </ModalView>
-                  </ModalBackdrop> 
-                  : null}
+              <a href={KAKAO_AUTH_URL}>
+                <div className="kakao_btn"></div>
+              </a>
+            </ModalView>
+          </ModalBackdrop>
+        ) : null}
       </ModalContainer>
     </>
   );
-}  
+}
 
-export default Login; 
+export default Login;
--- client/src/components/Modal/Menu/Menu.js
+++ client/src/components/Modal/Menu/Menu.js
@@ -1,18 +1,15 @@
-import React, { useState, useHistory } from 'react';
-import Modal from 'react-modal';
-import styled from 'styled-components';
-
-Modal.setAppElement('#root');
+import React, { useState, useHistory } from "react";
+import styled from "styled-components";
 
 export const ModalContainer = styled.div`
-// TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
+  // TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
   text-align: center;
   width: 100%;
   height: 100%;
 `;
 
 export const ModalBackdrop = styled.div`
- // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
+  // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
   position: fixed;
   display: flex;
   justify-content: center;
@@ -28,7 +25,6 @@ export const ModalBackdrop = styled.div`
   z-index: 999; // 레이어의 맨 위에 모달 창이 나와야 하므로 가장 큰 수(999)로 설정
 `;
 
-
 export const ModalBtn = styled.button`
   background-color: #4000c7;
   text-decoration: none;
@@ -39,9 +35,9 @@ export const ModalBtn = styled.button`
   cursor: grab;
 `;
 
-export const ModalView = styled.div.attrs(props => ({
+export const ModalView = styled.div.attrs((props) => ({
   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
-  role: 'dialog'
+  role: "dialog",
 }))`
   display: flex;
   flex-direction: column;
@@ -52,29 +48,31 @@ export const ModalView = styled.div.attrs(props => ({
   height: 100px;
   border-radius: 1rem;
   position: relative;
-  > div.close-btn{
+  > div.close-btn {
     position: absolute;
-    top:2px;
-    right:7px;
+    top: 2px;
+    right: 7px;
     cursor: pointer; // 마우스포인터를 위에 올리면 커서가 손 모양으로 변하게
-  > div.desc {
-    color: violet;
-    margin-top: 25px;
-  }
+    > div.desc {
+      color: violet;
+      margin-top: 25px;
+    }
   }
 `;
 
-function Menu () {
+function Menu() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const openModalHandler = () => {
-    setModalIsOpen(!modalIsOpen)
+    setModalIsOpen(!modalIsOpen);
   };
   // const history = useHistory('');
-    // 모달 창이 열렸을 때, 스크롤 막기
-    if (modalIsOpen) {
-      document.body.style.overflow = "hidden";
-    } else { document.body.style.overflow = "unset" }
-     
+  // 모달 창이 열렸을 때, 스크롤 막기
+  if (modalIsOpen) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "unset";
+  }
+
   return (
     <>
       <ModalContainer onClick={openModalHandler}>
@@ -83,24 +81,26 @@ function Menu () {
           MENU
         </ModalBtn>
 
-        { modalIsOpen ? <ModalBackdrop onClick={openModalHandler}>
-                    <ModalView onClick={(event) => {
-                      event.stopPropagation()
-                    }
-                  }>
-                      <div className="close-btn" onClick={openModalHandler}>
-                      &times;
-                      </div>
-                      <div className="desc">MENU</div>
-                      <div className="desc">마이페이지</div>
-                      <div className="desc">브루어리 찾기</div>
-                      <div className="desc">찜한 목록</div>
-                    </ModalView>
-                  </ModalBackdrop> 
-                  : null}
+        {modalIsOpen ? (
+          <ModalBackdrop onClick={openModalHandler}>
+            <ModalView
+              onClick={(event) => {
+                event.stopPropagation();
+              }}
+            >
+              <div className="close-btn" onClick={openModalHandler}>
+                &times;
+              </div>
+              <div className="desc">MENU</div>
+              <div className="desc">마이페이지</div>
+              <div className="desc">브루어리 찾기</div>
+              <div className="desc">찜한 목록</div>
+            </ModalView>
+          </ModalBackdrop>
+        ) : null}
       </ModalContainer>
     </>
-  )
-}  
+  );
+}
 
-export default Menu; 
+export default Menu;
