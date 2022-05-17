5ae6f1ed23b8b83c5afa93389fe0f6b0560cacdc
YeonSeoJo <irwinjoebeck@gmail.com>
2022-05-17 14:45:32 +0900

    port_changed_and_db_connected

--- server/index.js
+++ server/index.js
@@ -5,9 +5,25 @@ const usersRouter = require('./router/usersRouter.js');
 const breweryRouter = require('./router/breweryRouter.js');
 const mypageRouter = require('./router/mypageRouter.js');
 
+const morgan = require('morgan');
+const path = require('path');
+
+const { sequelize } = require('./models');
+//데이터 베이스 연결을 위해 추가
+
+
 app.get('/', function(req, res) { 
   res.send('Hello World!'); 
 });
+
+sequelize.sync({ force: false})
+  .then(() => {
+    console.log('데이터베이스 연결 성공')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+  //데이터베이스 연결 위해 추가
 
 app.post("/signup", controllers.signup);
 app.post("/login", controllers.login);
@@ -21,7 +37,8 @@ app.use('/users', usersRouter);
 app.use('/:breweryid', breweryRouter);
 app.use('/mypage', mypageRouter);
 
-let port = 3000; 
+let port = 8080;
+//포트 겹치지 않기 위해 임시 변경 
 app.listen(port, function(){ 
   console.log('server on! http://localhost:'+port); 
 });
